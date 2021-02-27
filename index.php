f25f3078eb4917b9f209cd8ca6526fa9f0b0ebc9
viveksoni100 <33828828+viveksoni100@users.noreply.github.com>
2021-02-27 22:31:54 +0530

    overlapping on 3rd screen is solved

--- index.php
+++ index.php
@@ -170,11 +170,11 @@
 
                                                 <!-- Layers -->
                                                 <div class="tp-caption tp-resizeme text-white font-heading font-weight-400 text-right" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-130','-130','-125','-165']" data-fontsize="['54','54','50','38']" data-lineheight="['64','64','60','48']" data-width="full" data-height="none" data-whitespace="normal" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;" data-start="700" data-splitin="none" data-splitout="none" data-responsive_offset="on">
-                                                    TYREOSHEILD™ designed for use
+                                                    TYREOSHEILD™ <br/> designed for use <br/>
                                                 </div>
 
                                                 <div class="tp-caption tp-resizeme text-white font-heading font-weight-700 text-right" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-62','-62','-62','-67']" data-fontsize="['54','54','50','38']" data-lineheight="['64','64','60','48']" data-width="full" data-height="none" data-whitespace="normal" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;" data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on">
-                                                    in domestic and commercial vehicles
+                                                    <br/><br/><br/> in domestic and commercial vehicles
                                                 </div>
 
                                             </li>
