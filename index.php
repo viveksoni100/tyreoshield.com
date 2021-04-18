c501a28fb9a646ba61acfc9d069db6cc9f2422eb
viveksoni100 <33828828+viveksoni100@users.noreply.github.com>
2021-04-18 13:47:10 +0530

    final touch is added

--- index.php
+++ index.php
@@ -177,7 +177,7 @@
                                                 </div>
 
                                                 <div class="tp-caption tp-resizeme text-white font-heading font-weight-700 text-right" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-62','-62','-62','-67']" data-fontsize="['54','54','50','38']" data-lineheight="['64','64','60','48']" data-width="full" data-height="none" data-whitespace="normal" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;" data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on">
-                                                    <br /><br /><br /> IN All Kind Of Vehicle vehicles
+                                                    <br /><br /><br /> IN All Kind Of vehicles
                                                 </div>
 
                                             </li>
@@ -1283,14 +1283,14 @@
                         <div class="bottom-bar-inner-wrap">
                             <div class="bottom-bar-content">
                                 <div id="copyright">
-                                    &copy; Tyreoshield - Design by <a href="http://viveksoni100.co.in" target="_blank">@Vivek Soni.</a>
+                                    &copy; Tyreoshield - All rights are reserved.
                                 </div><!-- /#copyright -->
                             </div><!-- /.bottom-bar-content -->
 
                             <div class="bottom-bar-menu">
                                 <ul class="bottom-nav">
                                     <li class="menu-item current-menu-item"><a href="#">Home</a></li>
-                                    <li class="menu-item"><a href="#">About Us</a></li>
+                                    <li class="menu-item"><a href="#">Contact Us</a></li>
                                 </ul>
                             </div><!-- /.bottom-bar-menu -->
                         </div>
